7d66bf9bde1f57d8282d85ef1ac32e6f5b1beba5
Gauthier Cart <gauthier.cart@gmail.com>
2022-12-03 13:38:03 +0100

    chore: delete front cypress apps

--- apps/trail-path-api/src/app/app.service.ts
+++ apps/trail-path-api/src/app/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-
     return {
       message: 'Welcome to trail-path-api lol!',
     };
