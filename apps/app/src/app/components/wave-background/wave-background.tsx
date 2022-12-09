5af022836df92a159a82df6aa16bb7918c0e7273
Gauthier Cart <gauthier.cart@gmail.com>
2022-12-09 17:23:45 +0100

    refactor: wave background

--- apps/app/src/app/components/wave-background/wave-background.tsx
+++ apps/app/src/app/components/wave-background/wave-background.tsx
@@ -1,4 +1,4 @@
-import { Container, SVG } from '@svgdotjs/svg.js';
+import { Container, Gradient, SVG } from '@svgdotjs/svg.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { random, map, spline } from '@georgedoescode/generative-utils';
@@ -7,80 +7,89 @@ import { canvas, canvasWrapper } from './wave-background.css';
 
 import React, { useEffect, useRef } from 'react';
 
+function lerp(v0: number, v1: number, t: number): number {
+  return v0 * (1 - t) + v1 * t;
+}
+
+function wave(
+  svg: Container,
+  height: number,
+  start: { x: number; y: number },
+  end: { x: number; y: number },
+  gradient: Gradient,
+) {
+  const numSteps = random(4, 8, true);
+  const randomRange = random(32, 64);
+
+  const points = [];
+
+  for (let i = 0; i <= numSteps; i++) {
+    const step = map(i, 0, numSteps, 0, 1);
+
+    let x = lerp(start.x, end.x, step);
+    let y = lerp(start.y, end.y, step);
+
+    if (i !== 0 && i !== numSteps) {
+      x += random(-randomRange, randomRange);
+      y += random(-randomRange, randomRange);
+    }
+
+    points.push({ x, y });
+  }
+
+  const pathData =
+    spline(points, 1, false) + `L ${end.x} ${height} L ${start.x} ${height} Z`;
+
+  svg.path(pathData).attr('fill', gradient);
+}
+
 export function WaveBackground() {
   const initializedRef = useRef(false);
-
-  function lerp(v0: number, v1: number, t: number): number {
-    return v0 * (1 - t) + v1 * t;
-  }
 
   useEffect(() => {
     if (document && !initializedRef?.current) {
       const svg: Container = SVG('.canvas') as unknown as Container;
       const { width, height } = svg.viewbox();
 
-      function wave(svg: Container, start, end, gradient) {
-        const numSteps = random(4, 8, true);
-        const randomRange = random(32, 64);
+      const numWaves = 7;
+      const base = tinycolor(`hsl(${random(220, 360)}, 65%, 55%)`);
+      const colors = base.analogous(6);
 
-        const points = [];
+      // noinspection TypeScriptValidateJSTypes
+      svg
+        .rect(width, height)
+        .fill(random(colors).clone().darken(40).toString());
 
-        for (let i = 0; i <= numSteps; i++) {
-          const step = map(i, 0, numSteps, 0, 1);
+      for (let i = 0; i < numWaves; i++) {
+        const randomOffset = random(-50, 50);
+        const originY =
+          map(i, 0, numWaves, -height / 2, height / 3) + randomOffset;
+        const endY = map(i, 0, numWaves, 0, 1000) + randomOffset;
 
-          let x = lerp(start.x, end.x, step);
-          let y = lerp(start.y, end.y, step);
+        const color = random(colors).clone();
 
-          if (i !== 0 && i !== numSteps) {
-            x += random(-randomRange, randomRange);
-            y += random(-randomRange, randomRange);
-          }
-
-          points.push({ x, y });
+        if (i < 3) {
+          color.darken(50).desaturate(10);
         }
 
-        const pathData =
-          spline(points, 1, false) +
-          `L ${end.x} ${height} L ${start.x} ${height} Z`;
+        const gradientOffset = map(i, 0, numWaves, 0.1, 1);
 
-        svg.path(pathData).attr('fill', gradient);
+        const gradient = svg.gradient('linear', function (add) {
+          add.stop(0, color.clone().lighten(30).toHexString());
+          add.stop(gradientOffset, color.clone().spin(60).toHexString());
+        });
+
+        gradient.from(0.5, 0).to(0, 1);
+
+        wave(
+          svg,
+          height,
+          { x: 0, y: originY },
+          { x: width, y: endY },
+          gradient,
+        );
       }
 
-      function generate() {
-        const numWaves = 7;
-        const base = tinycolor(`hsl(${random(220, 360)}, 65%, 55%)`);
-        const colors = base.analogous(6);
-
-        svg
-          .rect(width, height)
-          .fill(random(colors).clone().darken(40).toString());
-
-        for (let i = 0; i < numWaves; i++) {
-          const randomOffset = random(-50, 50);
-          const originY =
-            map(i, 0, numWaves, -height / 2, height / 3) + randomOffset;
-          const endY = map(i, 0, numWaves, 0, 1000) + randomOffset;
-
-          const color = random(colors).clone();
-
-          if (i < 3) {
-            color.darken(50).desaturate(10);
-          }
-
-          const gradientOffset = map(i, 0, numWaves, 0.1, 1);
-
-          const gradient = svg.gradient('linear', function (add) {
-            add.stop(0, color.clone().lighten(30).toHexString());
-            add.stop(gradientOffset, color.clone().spin(60).toHexString());
-          });
-
-          gradient.from(0.5, 0).to(0, 1);
-
-          wave({ x: 0, y: originY }, { x: width, y: endY }, gradient);
-        }
-      }
-
-      generate();
       initializedRef.current = true;
     }
   }, []);
--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import ViteTsConfigPathsPlugin from 'vite-tsconfig-paths';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { VitePluginFonts } from 'vite-plugin-fonts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
@@ -36,6 +37,7 @@ export default defineConfig({
         injectTo: 'head-prepend',
       },
     }),
+    tsconfigPaths(),
     vanillaExtractPlugin({}),
   ],
 });
